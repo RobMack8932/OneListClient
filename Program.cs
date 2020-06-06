83612d72c007f5886d44af8e928b10598cff8ac2
Rob Mack <robmack8932@icloud.com>
2020-06-06 00:26:42 -0400

    OneListClient

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace OneListClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    class Item
     {
-      Console.WriteLine("Welcome to C#");
+        public int id { get; set; }
+        public string type { get; set; }
+        public string setup { get; set; }
+        public string punchline { get; set; }
     }
-  }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseAsStream = await client.GetStreamAsync(
+            "https://official-joke-api.appspot.com/random_ten"
+            );
+            var items = await JsonSerializer.DeserializeAsync<List<Item>>(responseAsStream);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.setup}.... { item.punchline}");
+            }
+        }
+    }
 }
